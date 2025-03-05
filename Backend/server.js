58eb1ac63bdfa9ba90b0dd5461344149268b0455
Yashraj8959 <ykushwah7828@gmail.com>
2025-03-05 14:20:47 +0530

    feat: implement Socket.IO for real-time communication and project-specific chat functionality

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,27 @@ const app = require('./src/app');
 const connect = require('./src/db/db');
 const PORT = process.env.PORT || 3030;
 connect();
-app.listen(PORT, () => {
+
+
+const Server = require('http').createServer(app);
+const io = require('socket.io')(Server,{
+    cors: {
+        origin: '*',
+    }
+});
+io.on('connection', (socket) => {
+    const projectId = socket.handshake.query.projectId;
+    console.log(`User connected to project ${projectId}`);
+    socket.join(projectId);    // join the user to the project room 
+    // console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        socket.to(projectId).emit('chat message', msg);   // send message to only the project user
+        // io.emit('chat message', msg);          // broadcast message to all users in the project
+    });
+});
+Server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
